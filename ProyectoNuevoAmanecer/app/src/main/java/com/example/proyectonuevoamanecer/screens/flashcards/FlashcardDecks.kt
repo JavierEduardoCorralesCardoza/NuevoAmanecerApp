644930d9e102426278abdfa9818981df38f2c38e
Facundo Marroquin <A01383541@tec.mx>
2023-11-01 17:51:52 -0600

    Flashcarddecks

--- ProyectoNuevoAmanecer/app/src/main/java/com/example/proyectonuevoamanecer/screens/flashcards/FlashcardDecks.kt
+++ ProyectoNuevoAmanecer/app/src/main/java/com/example/proyectonuevoamanecer/screens/flashcards/FlashcardDecks.kt
@@ -3,12 +3,28 @@ package com.example.proyectonuevoamanecer.screens.flashcards
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.proyectonuevoamanecer.ui.theme.ProyectoNuevoAmanecerTheme
 
 class FlashcardDecks  : ComponentActivity() {
@@ -27,8 +43,38 @@ class FlashcardDecks  : ComponentActivity() {
         }
     }
 }
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun DecksFlashcards(){
+    var selectedDeck by remember { mutableStateOf<String?>(null) }
+    Text(text="Mazos",
+        style = TextStyle(fontSize=24.sp,fontWeight = FontWeight.Bold),
+        modifier = Modifier.padding(16.dp).fillMaxWidth(),
+        textAlign= TextAlign.Center)
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
 
+    ) {
+        Button(
+            onClick = { /* Acción para seleccionar el primer mazo */ },
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text("Mazo 1")
+        }
+
+        Button(
+            onClick = { /* Acción para seleccionar el segundo mazo */ },
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text("Mazo 2")
+        }
+
+        // Otros botones de mazos aquí...
+
+        if (selectedDeck != null) {
+            // Mostrar un mensaje o navegar a la actividad que muestra las tarjetas del mazo seleccionado
+        }
+    }
 }
--- ProyectoNuevoAmanecer/app/src/main/java/com/example/proyectonuevoamanecer/screens/flashcards/MainFlashMenu.kt
+++ ProyectoNuevoAmanecer/app/src/main/java/com/example/proyectonuevoamanecer/screens/flashcards/MainFlashMenu.kt
@@ -6,7 +6,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -17,6 +19,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
@@ -53,21 +56,15 @@ class MainFlashMenu  : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun FlashcardMenu( onDeckCheckClick:() -> Unit = {}, onBackClick: () -> Unit = {}){
+    Text(text="Tarjetas Educativas",
+        style = TextStyle(fontSize=24.sp,fontWeight = FontWeight.Bold),
+        modifier = Modifier.padding(16.dp).fillMaxWidth(),
+        textAlign= TextAlign.Center)
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
         content = {
-            Text(text="Tarjetas Educativas",
-                style = TextStyle(fontSize=24.sp,fontWeight = FontWeight.Bold),
-                modifier = Modifier.padding(16.dp))
-            Button(
-                onClick = { onBackClick() },
-                modifier = Modifier.padding(16.dp)
-            ) {
-                // Contenido del botón "Regresar"
-                Text("Regresar")
-            }
 
             Button(
                 onClick = { onDeckCheckClick() },
@@ -75,6 +72,13 @@ fun FlashcardMenu( onDeckCheckClick:() -> Unit = {}, onBackClick: () -> Unit = {
             ) {
                 // Contenido del botón "Verificar Mazos"
                 Text("Ver Mazos")
+            }
+            Button(
+                onClick = { onBackClick() },
+                modifier = Modifier.padding(16.dp)
+            ) {
+                // Contenido del botón "Regresar"
+                Text("Regresar")
             }
         }
     )
