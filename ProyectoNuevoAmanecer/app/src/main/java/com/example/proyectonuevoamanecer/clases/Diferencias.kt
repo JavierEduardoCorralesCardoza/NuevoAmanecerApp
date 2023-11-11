01584d41da908a493160c4dc7caa4a7dbff66218
Jose David De la Garza Salas <jdgarza11@hotmail.com>
2023-11-10 18:45:37 -0600

    Logica finalizada_diferencias

--- ProyectoNuevoAmanecer/app/src/main/java/com/example/proyectonuevoamanecer/clases/Diferencias.kt
+++ ProyectoNuevoAmanecer/app/src/main/java/com/example/proyectonuevoamanecer/clases/Diferencias.kt
@@ -2,20 +2,15 @@ package com.example.proyectonuevoamanecer.clases
 
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 
 class Diferencias(
-
     var image1: Int,
     var image2: Int,
     var name: String,
     var differenceNumber: Int,
-    differences: MutableList<Int>
+    differences: MutableList<BotonDiferencias>
 ){
     var differences by mutableStateOf(differences)
-}
-
-
-fun getDifferences() {
-
 }
