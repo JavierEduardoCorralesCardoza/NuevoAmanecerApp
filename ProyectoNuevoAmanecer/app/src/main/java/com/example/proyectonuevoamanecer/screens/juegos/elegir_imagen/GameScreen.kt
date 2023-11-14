627f1626f1b0ee97d504006dbc61f76de6f30234
A01411926 <a01411926@tec.mx>
2023-11-13 19:34:28 -0600

    Juego de imagenes

---
+++ ProyectoNuevoAmanecer/app/src/main/java/com/example/proyectonuevoamanecer/screens/juegos/elegir_imagen/GameScreen.kt
@@ -0,0 +1,88 @@
+package com.example.juego1jetpc.ui
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.proyectonuevoamanecer.R
+
+@Composable
+fun GameScreen() {
+    val images = listOf(
+        R.drawable.cat,
+        R.drawable.banana,
+        R.drawable.blueberries
+    )
+    val imageLabels = listOf("Gato", "Banana", "Mora")
+    val correctAnswerIndex = 0 // índice de la imagen correcta
+
+    var selectedAnswer by remember { mutableStateOf(-1) }
+    var isCorrect by remember { mutableStateOf(false) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ) {
+        Text(
+            text = "Selecciona la imagen correcta",
+            fontSize = 20.sp, // Aumenta el tamaño del texto de la instrucción
+            fontWeight = FontWeight.Bold, // Pone el texto en negrita
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 8.dp) // Agrega espacio debajo del texto de la instrucción
+        )
+        Text(
+            text = "Gato", // La palabra que va justo debajo de la instrucción
+            fontSize = 18.sp, // Tamaño del texto para "Gato"
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 32.dp) // Agrega espacio debajo de la palabra "Gato"
+        )
+
+        images.forEachIndexed { index, imageRes ->
+            Image(
+                painter = painterResource(id = imageRes),
+                contentDescription = imageLabels[index], // Usa la etiqueta correspondiente para cada imagen
+                modifier = Modifier
+                    .size(150.dp) // Aumenta el tamaño de las imágenes
+                    .padding(8.dp) // Añade relleno alrededor de las imágenes
+                    .clickable {
+                        selectedAnswer = index
+                        isCorrect = index == correctAnswerIndex
+                    }
+            )
+        }
+
+        if (selectedAnswer >= 0) {
+            Text(
+                text = if (isCorrect) "¡Correcto!" else "Inténtalo de nuevo",
+                fontSize = 18.sp, // Aumenta el tamaño del texto de retroalimentación
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp) // Añade espacio encima del texto de retroalimentación
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewGameScreen() {
+    GameScreen()
+}
---
+++ ProyectoNuevoAmanecer/app/src/main/res/drawable/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.juego1jetpc
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.juego1jetpc.ui.theme.Juego1jetpcTheme
+import com.example.juego1jetpc.ui.GameScreen
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Juego1jetpcTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    GameScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Juego1jetpcTheme {
+        Greeting("Android")
+    }
+}
